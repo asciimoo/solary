4d2a6a3fee57fa2ef2e76e50edaf68d95174fbd0
Adam Tauber <asciimoo@gmail.com>
2016-01-30 17:27:08 +0100

    [enh] command line switch for players number

--- server.go
+++ server.go
@@ -9,17 +9,13 @@ import (
 	"github.com/asciimoo/solary/arena/board"
 )
 
-const (
-	PLAYERS_NUM int = 2
-)
-
-func gameLoop(address string) {
+func gameLoop(address string, players_num int) {
 	game_server, _ := net.Listen("tcp", address)
 	fmt.Println("Server started")
 
 	for {
 		a := arena.Create()
-		for i := 0; i < PLAYERS_NUM; i++ {
+		for i := 0; i < players_num; i++ {
 			conn, _ := game_server.Accept()
 			a.AddPlayer(conn)
 		}
@@ -31,13 +27,14 @@ func gameLoop(address string) {
 func main() {
 
 	address := flag.String("listen", "127.0.0.1:6666", "server listen address")
+	players_per_game := flag.Int("players", 2, "number of players in one arena")
 
 	flag.Parse()
 	if len(flag.Args()) != 0 {
 		b := board.Create()
 		fmt.Println(b)
 	} else {
-		gameLoop(*address)
+		gameLoop(*address, *players_per_game)
 	}
 
 }
